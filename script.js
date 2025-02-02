537a0cab3cd08ad1db85b66d88eca9b9ac26c118
unknown <yeicod1218@gmail.com>
2025-02-02 13:51:01 -0500

    ''espanol''

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Estas segura?",
+    "Segura Segurisima?",
+    "Sin remordimientos?",
+    "PORQUE NO",
+    "mi corazonzito de pollo no aguantara",
+    "si sigues diciendo no estare triste...",
+    "Muy muy triste...",
+    "pero muy muy muy muy troste...",
+    "ya po dejo de insistir...",
+    "mentiritas ACEPTAAAAAA ❤️"
 ];
 
 let messageIndex = 0;
